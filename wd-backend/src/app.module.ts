a4739d87433381a1079bbfff756beaa9e4eaa756
Sittinon Dormae <6810110373@psu.ac.th>
2026-02-16 23:14:58 +0700

    docker_setup

--- wd-backend/src/app.module.ts
+++ wd-backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'password123',
+      database: 'NewLearningAcademy_dev',
+      entities: [], // เราจะเพมิ่ Entities ทนี่ ี่ในภายหลัง
+      synchronize: true, // สรา้ง Table อัตโนมตั ิ(ใชสำ้ สำ หรบั Dev เทา่ นั้น)
+}),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
