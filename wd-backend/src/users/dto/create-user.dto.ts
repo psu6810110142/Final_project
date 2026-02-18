ef832997b5130afa2bb006b24f718230028db1ed
psu6810110476 <6810110476@psu.ac.th>
2026-02-18 14:28:03 +0700

    users_API

--- wd-backend/src/users/dto/create-user.dto.ts
+++ wd-backend/src/users/dto/create-user.dto.ts
@@ -1,1 +1,11 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+  username: string;
+  password_hash: string;
+  full_name: string;
+  email: string;
+  phone?: string;
+  profile_picture_url?: string;
+  interesting_subject?: string;
+  role?: 'STUDENT' | 'ADMIN';
+  level_id?: number; 
+}
--- wd-backend/src/users/users.service.ts
+++ wd-backend/src/users/users.service.ts
@@ -1,26 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>,
+  ) {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const newUser = this.userRepo.create(createUserDto);
+    
+    if (createUserDto.level_id) {
+        newUser.level = { level_id: createUserDto.level_id } as any;
+    }
+
+    return this.userRepo.save(newUser);
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.userRepo.find({
+      relations: ['level'], 
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    const user = await this.userRepo.findOne({ 
+      where: { user_id: id },
+      relations: ['level']
+    });
+    
+    if (!user) throw new NotFoundException(`ไม่พบผู้ใช้งานรหัส ${id}`);
+    
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async findByUsername(username: string) {
+    return this.userRepo.findOne({ where: { username } });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = await this.findOne(id);
+    
+    Object.assign(user, updateUserDto);
+    
+    if (updateUserDto.level_id) {
+        user.level = { level_id: updateUserDto.level_id } as any;
+    }
+
+    return this.userRepo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    return this.userRepo.remove(user);
   }
 }
