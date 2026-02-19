e7652afa5d993595dc755d580975ac8ccc660f6a
Sittinon Dormae <6810110373@psu.ac.th>
2026-02-19 16:13:28 +0700

    orders_Authorization

--- wd-backend/src/orders/orders.controller.ts
+++ wd-backend/src/orders/orders.controller.ts
@@ -1,37 +1,49 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
 
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
+  @UseGuards(AuthGuard('jwt'))
   @Post() // เปิดบิล
   create(@Body() createOrderDto: CreateOrderDto) {
     return this.ordersService.create(createOrderDto);
   }
 
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles('ADMIN')
   @Get() // ดูทั้งหมด (Admin)
   findAll() {
     return this.ordersService.findAll();
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get('user/:userId') // ✨ ดูประวัติการสั่งซื้อของ User คนนี้
   findByUser(@Param('userId') userId: string) {
     return this.ordersService.findByUser(+userId);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.ordersService.findOne(+id);
   }
 
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles('ADMIN')
   @Patch(':id') // แก้ไขสถานะออเดอร์
   update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
     return this.ordersService.update(+id, updateOrderDto);
   }
 
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles('ADMIN')
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.ordersService.remove(+id);
