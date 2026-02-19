a557f4c858d772c0f1525133b4303b074dc782b4
Thanakon Khuadreem <thanakon69798@gmail.com>
2026-02-20 00:47:47 +0700

    แก้ไขชื่อให้ตรงกับfrontend

--- wd-backend/src/main.ts
+++ wd-backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+  app.enableCors();
   // ✨ เปลี่ยนจาก 3000 เป็น 3001 เพื่อหนี Port ที่ชนกัน
   await app.listen(process.env.PORT ?? 3001); 
   
