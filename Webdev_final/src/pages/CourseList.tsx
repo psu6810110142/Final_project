5d258fb139b3008d3972b4710709b4a5f72e32b8
Sittinon Dormae <6810110373@psu.ac.th>
2026-03-03 00:52:11 +0700

    uploadCoverImage_system_for_Courses(FIXED)

--- Webdev_final/src/pages/CourseList.tsx
+++ Webdev_final/src/pages/CourseList.tsx
@@ -19,6 +19,7 @@ interface CourseData {
   };
 }
 
+//เพิ่มฟังก์ชันนี้ไว้ด้านบนสุดของไฟล์
 const getImageUrl = (url?: string) => {
   if (!url) return "https://images.unsplash.com/photo-1516321318423-f06f85e504b3?q=80&w=400"; // รูป Default
   if (url.startsWith('/uploads')) {
